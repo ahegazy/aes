b6f8f13e2df744c21ce5f99639014bf6cee57a25
Ahmad Hegazy <ahegazipro@gmail.com>
2018-09-13 19:40:27 +0200

    redundant

--- keyExp.v
+++ keyExp.v
@@ -1,6 +1,7 @@
-module key_expansionH( 
+module key_expansion( 
   input  [127:0] key_0 ,
 	input clk,
+	input reset,
   output  reg [127:0] key_1 , 
   output  reg [127:0] key_2 ,
   output  reg [127:0] key_3 ,
@@ -16,48 +17,49 @@ module key_expansionH(
   wire [31:0] stp2 ;
   wire [31:0]Rcon ;
 	reg [3:0] m,n; 
-	
-    //  integer n;
-    //   a,b,c,d;
-  // array to store all keys in it
-   reg [127:0] k [10:0];
-
-  //1D to 2D for input key
-  initial
-  begin
-  m <= 0;
-	n <= 1;
-	k[0] <= key_0;
-	k[1] <= 128'b0;
-	k[2] <= 128'b0;
-	k[3] <= 128'b0;
-	k[4] <= 128'b0;
-	k[5] <= 128'b0;
-	k[6] <= 128'b0;
-	k[7] <= 128'b0;
-	k[8] <= 128'b0;
-	k[9] <= 128'b0;
-	k[10] <= 128'b0;
-		end
+	reg [127:0] k [10:0];
 
  	 rcon uut1 (.r(m),.rcon(Rcon));
    subByte uut2 (.state(stp1) , .state_out(stp2));
 	 
 	 always @(negedge clk)
 	 begin 
-  $display("key %d : %h, %h",m,k[m],{ k[m][25:0] , k[m][31:24] });
- 	  stp1 	<= { k[n-1][25:0] , k[n-1][31:24] }; 
+		if( reset == 1 )
+		begin 
+			m <= 0;
+		end
+		else 
+		begin 		
+	 $display("key %d : %h, %h",m,k[m],{ k[m][25:0] , k[m][31:24] });
+ 	  stp1 	<= { k[m][25:0] , k[m][31:24] }; 
 	   m <= m + 1;        // m= 0 1 2 3 4 5 6 7 8 9 
-	   n <= n+1; /*m 0 n 1*/ //n=  1 2 3 4 5 6 7  8 9 10 
 	   end 
+		 end 
 
 always @(posedge clk) 
  begin
-
+	if ( reset == 1 )
+	begin 
+		k[0] <= key_0;
+		k[1] <= 128'b0;
+		k[2] <= 128'b0;
+		k[3] <= 128'b0;
+		k[4] <= 128'b0;
+		k[5] <= 128'b0;
+		k[6] <= 128'b0;
+		k[7] <= 128'b0;
+		k[8] <= 128'b0;
+		k[9] <= 128'b0;
+		k[10] <= 128'b0;
+	end 
+	else 
+	begin 
 		k[m][127:96] = k[m-1][127:96] ^ stp2 ^ Rcon; 	
 		k[m][95:64] = k[m-1][95:64] ^ k[m][127:96];
 		k[m] [63:32] = k[m-1][63:32] ^ k[m][95:64];
 		k[m][31:0] = k[m-1][31:0] ^ k[m][63:32];
+		
+	end 
 end
  endmodule
 
