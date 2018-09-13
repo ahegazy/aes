b4b5746b9415862941570bdc2371d0ce656c699f
Esraa Gaber <esraagaber2397@gmail.com>
2018-09-13 12:04:27 +0200

    Update key_exp.v

--- key_exp.v
+++ key_exp.v
@@ -1,5 +1,6 @@
 module key_expansion ( 
   input  [127:0] key_0 ,
+  input en ,
   output reg [127:0] key_1 , 
   output reg [127:0] key_2 ,
   output reg [127:0] key_3 ,
@@ -14,33 +15,47 @@ module key_expansion (
   reg [31:0] stp1 ;
   wire [31:0] stp2 ;
   wire [31:0]Rcon ;
-      integer n,m;
-      reg a,b,c,d;
+  integer n,m;
+  
   // array to store all keys in it
   reg [31:0] k [0:43];
   
-  //1D to 2D for input key
-  initial
-  begin
-    k[0][31:0] = key_0 [127:96];
-    k[1][31:0] = key_0 [95:64];
-    k[2][31:0] = key_0 [63:32];
-    k[3][31:0] = key_0 [31:0];
-  end
-  
     rcon uut1 (.r(m),.rcon(Rcon));
    subByte uut2 (.state(stp1) , .state_out(stp2));
+   
   
-  initial
-  begin
-
+  always@( en)
+    begin
+		k[0][31:0] = key_0 [127:96];
+		k[1][31:0] = key_0 [95:64];
+		k[2][31:0] = key_0 [63:32];
+		k[3][31:0] = key_0 [31:0];
+        stp1= {key_0[25:0],key_0[31:24]};
+        $display("%b",stp1);
+        $display("%t",$time,);
+		if (en==0)
+		  begin
+		    key_1 =0; 
+        key_2 =0; 
+        key_3 =0;
+        key_4 =0;
+        key_5 =0;
+        key_6 =0;
+        key_7 =0;
+        key_8 =0;
+        key_9 =0;
+        key_10=0 ;
+      end
+		else
+		begin
+		  //stp1= {key_0[25:0],key_0[31:24]};
     for (n=4 ; n<=43 ; n=n+1)
     begin
       m=n/4;
       if ((n%4)==0)
       begin
         stp1= {k[n-1][25:0],k[n-1][31:24]} ;
-        #10;
+        //#10;
         k[n] = k[n-4] ^ stp2 ^ Rcon ;
       end
     else
@@ -60,9 +75,12 @@ module key_expansion (
       key_8 ={k[32],k[33],k[34],k[35]} ;
       key_9 ={k[36],k[37],k[38],k[39]} ;
       key_10 ={k[40],k[41],k[42],k[43]} ;
-  
+    
     end
+  end
   end
   
   endmodule
+
+
 
