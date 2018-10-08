d6ddfb9257340d08d6721df1fe49ae2a2e89b10a
Ahmad Hegazy <ahegazipro@gmail.com>
2018-10-08 20:32:49 +0200

    sepatrating functions inputs/outputs

--- tb/aesEnc_tb_con.sv
+++ tb/aesEnc_tb_con.sv
@@ -11,11 +11,13 @@
 
 class AES_ENC;
 
-	bit [0:127] key;
-	bit [0:127] state;
-	bit [0:127] state_out;
+	reg [0:127] key;
+	reg [0:127] state;
+	reg [0:127] state_out;
+	protected bit [7:0] state_2d [3:0] [3:0]; 
+	protected bit [7:0] key_2d [3:0] [3:0];
 	
-	protected bit [7:0] polymat [0:3] [0:3];
+	protected bit [7:0] polymat [3:0] [3:0];
 	protected bit [7:0] sbox [ bit [7:0] ];	
 	protected bit [7:0] rcon [ bit [3:0] ];
 	
@@ -24,20 +26,40 @@ class AES_ENC;
 	endfunction
 
 	function void init();
-
-	/* polynomial matrix */
-		this.polymat = {{ 8'h02,8'h03,8'h01,8'h01} , {8'h01,8'h02,8'h03,8'h01} , {8'h01,8'h01,8'h02,8'h03} , {8'h03,8'h01,8'h01,8'h02}};
-	/* Generate S_box */
+			int i,j,ij,ijs;
+		/* convert 1Dimension to 2 Dimension*/
+		for (i=0;i<=3;i=i+1)
+			for(j=0;j<=3;j=j+1)
+				begin 
+					ij = (i*4+j)*8; /* if input is [0:127]*/
+					//ijs=(15-(i*4+j))*8; /* if input is [127:0]*/
+					this.state_2d[j][i] = this.state[ij+: 8];
+					this.key_2d[i][j] = this.key[ij+: 8];
+				end
+		/* polynomial matrix */
+//		this.polymat = {{ 8'h02,8'h03,8'h01,8'h01} , {8'h01,8'h02,8'h03,8'h01} , {8'h01,8'h01,8'h02,8'h03} , {8'h03,8'h01,8'h01,8'h02}};
+		this.polymat = {{ 8'h02,8'h01,8'h01,8'h03} , {8'h03,8'h02,8'h01,8'h01} , {8'h01,8'h03,8'h02,8'h01} , {8'h01,8'h01,8'h03,8'h02}};		/* Generate S_box */
 		this.generate_SBOX();
 		/* Generate Rcon*/
 		this.generate_RCON();
 	endfunction
 	
+
+	function void finish();
+			int i,j,ij;
+		/* convert 2 Dimensions to 1 Dimension*/
+		for (i=0;i<=3;i=i+1)
+			for(j=0;j<=3;j=j+1)
+				begin 
+					ij = (i*4+j)*8;
+					this.state_out[ij+: 8] = this.state_2d[i][j];
+				end
+	endfunction
+
 		protected function bit [7:0] ROTL8( bit [7:0] x , bit [3:0] shift );
 				ROTL8 = ( ((x) << (shift)) | ((x) >> (8 - (shift))) );
 		endfunction
-	
-	protected function void generate_SBOX();
+		protected function void generate_SBOX();
 			bit [7:0] p = 1;
 			bit [7:0]	q = 1;
 			bit [7:0] xformed;
@@ -111,38 +133,19 @@ class AES_ENC;
 			mod = y;
 		endfunction
 		
-		function bit [0:127] ExpandKey (bit [0:127] key,bit [3:0] num);// bit [7:0] key_2d [3:0] [3:0]
-			int i,j,ij;
-		automatic bit [7:0] key_2d [3:0] [3:0];
+		function void ExpandKey (bit [3:0] num);// bit [7:0] key_2d [3:0] [3:0]
+		int i,j,k;
 		automatic bit [7:0] key_out_2d [3:0] [3:0];
-		automatic bit [0:127] key_out;
-		
-		/* convert 1Dimension to 2 Dimension*/
-		for (i=0;i<=3;i=i+1)
-			for(j=0;j<=3;j=j+1)
-				begin 
-					ij = (i*4+j)*8; /* if input is [0:127]*/
-					key_2d[i][j] = key[ij+: 8];
-				end
 		
  		for (i=0;i<=3;i++)
 			for(j=0;j<=3;j++)
 			begin 
 				if(i == 0)
 				begin 
-						key_out_2d[i][j] = 	key_2d[i][j] ^ this.SubByte(key_2d[3][(j == 3) ? 0 : j+1]) ^ ((j == 0) ? this.rcon[num] : 8'h00);
+						key_out_2d[i][j] = 	this.key_2d[i][j] ^ this.SubByte(this.key_2d[3][(j == 3) ? 0 : j+1]) ^ ((j == 0) ? this.rcon[num] : 8'h00);
 				end 
-				else key_out_2d[i][j] = key_2d[i][j] ^ key_out_2d[i-1][j];
+				else key_out_2d[i][j] = this.key_2d[i][j] ^ key_out_2d[i-1][j];
 			end 
-			
-		for (i=0;i<=3;i=i+1)
-			for(j=0;j<=3;j=j+1)
-				begin 
-					ij = (i*4+j)*8;
-					key_out[ij+: 8] = key_out_2d[i][j];
-				end
-
-			ExpandKey = key_out;				
 		endfunction
 		
 		function bit [7:0] SubByte (	bit [7:0] subitem );//, bit [7:0] key_2d [3:0] [3:0]
@@ -154,32 +157,20 @@ class AES_ENC;
 				SubByte_32[i*8+:8] = this.SubByte[subitem[i*8+:8]];
 		endfunction
 */
-		function bit [127:0] mix_columns (bit [127:0] state);//	bit [7:0] state_2d [3:0] [3:0]
-			int i,j,ij,k;
-			automatic bit [7:0] state_2d [0:3] [0:3];
-			automatic bit [7:0] mix_out_2d [0:3] [0:3];
+		function void mix_columns ();//	bit [7:0] state_2d [3:0] [3:0]
+			int i,j,k;
+			automatic bit [7:0] mix_out_2d [3:0] [3:0];
 			automatic bit [14:0] tmp_mult,tmp_mod;
-			automatic bit [127:0] state_out;
-
-			
 			tmp_mod = 15'h0;
 			mix_out_2d = {{8'h0,8'h0,8'h0,8'h0},{8'h0,8'h0,8'h0,8'h0},{8'h0,8'h0,8'h0,8'h0},{8'h0,8'h0,8'h0,8'h0}};/* init zero */
-			
-			for ( i=0; i<=3; i=i+1)
-				for ( j=0; j<=3; j=j+1)
-					begin
-					ij=15-(i*4+j);
-					state_2d[j][i]=state[ij*8  +: 8];
-				end	
-
 			for (i=0;i<=3;i++)
 				for(j=0;j<=3;j++)
 				begin 
-						tmp_mod=15'd0;
+				tmp_mod = 0;
 						/* scalar multiplication of two matrices */
 						for (k=0;k<=3;k++)
 						begin 
-								tmp_mult = this.finite_multiplication(state_2d[k][j],this.polymat[i][k] );
+								tmp_mult = this.finite_multiplication(this.state_2d[k][j],this.polymat[i][k] );
 								tmp_mod = tmp_mod ^ tmp_mult;
 								mix_out_2d[i][j] = this.mod(tmp_mod);
 						end
@@ -188,82 +179,55 @@ class AES_ENC;
 			$display("Mixed data",);				
 			for ( i=0; i<=3; i=i+1)
 				$display("%x %x %x %x ",mix_out_2d[i][0],mix_out_2d[i][1],mix_out_2d[i][2],mix_out_2d[i][3]);			
-
-				//2D to 1D
-
-			for ( i=0; i<=3; i=i+1) 
-				for ( j=0; j<=3; j=j+1)
-					begin
-						ij=15-(i*4+j);
-						state_out[ij*8  +:  8]=mix_out_2d[j][i];
-						end	
-			
-			mix_columns = state_out;
 	endfunction
 
 
-	function bit [0:127] ShiftRows (bit [0:127] state);//	bit [7:0] state_2d [3:0] [3:0]
-		int i,j,ij;
+	function void ShiftRows ();//	bit [7:0] state_2d [3:0] [3:0]
+		int i,j;
 		automatic bit [7:0] shift_out_2d [3:0] [3:0];
-		automatic bit [7:0] state_2d [0:3] [0:3];
-		automatic bit [0:127] state_out;
-		
-		/* 1d to 2d */
-		for ( i=0; i<=3; i=i+1)
-	   for ( j=0; j<=3; j=j+1)
-	      begin 
-	        ij =((4*i)+j);
-		      state_2d[j][i]=state[ij*8  +:  8];
-		    end	
 
 		for ( i=0; i<=3; i=i+1)
 	    for ( j=0; j<=3; j=j+1)
 	     begin
 	      if (i ==0)
-	         shift_out_2d[i][j] = state_2d[i][j];
+	         shift_out_2d[i][j] = this.state_2d[i][j];
 	      else if (i ==1)
 	        begin
-	         shift_out_2d[1][0] = state_2d[1][1];
-           shift_out_2d[1][1] = state_2d[1][2];
-           shift_out_2d[1][2] = state_2d[1][3];
-           shift_out_2d[1][3] = state_2d[1][0];
+	         shift_out_2d[1][0] = this.state_2d[1][1];
+           shift_out_2d[1][1] = this.state_2d[1][2];
+           shift_out_2d[1][2] = this.state_2d[1][3];
+           shift_out_2d[1][3] = this.state_2d[1][0];
           end
          else if (i ==2)
            begin
-	          shift_out_2d[2][0] = state_2d[2][2];
-	          shift_out_2d[2][1] = state_2d[2][3];
-            shift_out_2d[2][2] = state_2d[2][0];
-            shift_out_2d[2][3] = state_2d[2][1];
+	          shift_out_2d[2][0] = this.state_2d[2][2];
+	          shift_out_2d[2][1] = this.state_2d[2][3];
+            shift_out_2d[2][2] = this.state_2d[2][0];
+            shift_out_2d[2][3] = this.state_2d[2][1];
            end
           else if (i ==3) 
            begin
-		        shift_out_2d[3][0] = state_2d[3][3];
-            shift_out_2d[3][1] = state_2d[3][0];
-            shift_out_2d[3][2] = state_2d[3][1];
-            shift_out_2d[3][3] = state_2d[3][2];
+		        shift_out_2d[3][0] = this.state_2d[3][3];
+            shift_out_2d[3][1] = this.state_2d[3][0];
+            shift_out_2d[3][2] = this.state_2d[3][1];
+            shift_out_2d[3][3] = this.state_2d[3][2];
            end
 //        $display("sft: %x",shift_out_2d[i][j]);
 				end 
+    
 		$display("shifted data",);				
 		for ( i=0; i<=3; i=i+1)
 			$display("%x %x %x %x ",shift_out_2d[i][0],shift_out_2d[i][1],shift_out_2d[i][2],shift_out_2d[i][3]);
-    /* 2d to 1d */
-						for ( i=0; i<=3; i=i+1)
-							for ( j=0; j<=3; j=j+1)
-								begin 
-									ij =((4*i)+j);
-									state_out[ij*8  +:  8]=shift_out_2d[j][i];
-								end
-			ShiftRows = state_out;
 	endfunction
 	
-	function bit [127:0] AddRoundKey (bit [127:0] state,bit [127:0] key);//	bit [7:0] state_2d [3:0] [3:0], bit [7:0] key_2d [3:0] [3:0]
-		int i;
-		automatic bit [127:0] state_out;
- 		for (i=0;i<=15;i++)
-				state_out[i*8+:8] = key[i*8+:8] ^ state[i*8+:8];
-
-		AddRoundKey = state_out;
+	function void AddRoundKey ();//	bit [7:0] state_2d [3:0] [3:0], bit [7:0] key_2d [3:0] [3:0]
+		int i,j,k;
+		automatic bit [7:0] state_out_2d [3:0] [3:0];
+ 		for (i=0;i<=3;i++)
+			for(j=0;j<=3;j++)
+			begin 
+				state_out_2d[i][j] = this.key_2d[i][j] ^ this.state_2d[i][j];
+			end 
 	endfunction
 
 	
@@ -279,13 +243,12 @@ begin
 	encTst = new; 
 	encTst.state = 128'h54776F204F6E65204E696E652054776F;
 	encTst.key = 128'h5468617473206D79204B756E67204675;
-//	encTst.state = 128'h632fafa2eb93c7209f92abcba0c0302b;
-
+	
 	encTst.init();
-	$display("%x",encTst.ExpandKey(encTst.key,1));
+	encTst.ExpandKey(1);
 
-	$display("%x",encTst.mix_columns(encTst.state));
-	$display("%x",encTst.ShiftRows(encTst.state));
+	encTst.mix_columns();
+	encTst.ShiftRows();
 	//encTst.AddRoundKey();
 
 end 
